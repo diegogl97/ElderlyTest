1804931aed85b3938ace64081001d1d5c7524474
diegogl97 <61208708+diegogl97@users.noreply.github.com>
2021-04-24 18:11:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/elderlytest/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.elderlytest
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // prepare our List view and RecyclerView (cells)
+        setupRecyclerView(item_list)
+    }
+
+    // recycler view is the list of cells
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+
+        // update individual cell when the Note data are modified
+        UserData.notes().observe(this, Observer<MutableList<UserData.Note>> { notes ->
+            Log.d(TAG, "Note observer received ${notes.size} notes")
+
+            // let's create a RecyclerViewAdapter that manages the individual cells
+            recyclerView.adapter = NoteRecyclerViewAdapter(notes)
+        })
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
